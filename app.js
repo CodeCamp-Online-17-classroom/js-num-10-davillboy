a20e2ae4fe10a09cebee0325494c27ee3c5f6000
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 21:14:02 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // write code here
+function convertDegreesToRadians() {
+            let degrees = prompt("ระบุตัวเลขที่จะแปลงค่า:");
+            
+        
+            if (degrees === null || degrees.trim() === "" || isNaN(parseFloat(degrees))) {
+              alert("ใส่เฉพาะตัวเลข");
+              return; 
+            }
+            
+            degrees = parseFloat(degrees); 
+            let radians = degrees * (Math.PI / 180); 
+            
+            alert(degrees + "องศา" + radians.toFixed(2) + "เรเดียน");
+            console.log(degrees + "องศา" + radians.toFixed(2) + "เรเดียน");
+          }
+        
+          convertDegreesToRadians();
